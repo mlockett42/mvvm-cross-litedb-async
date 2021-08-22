9dcc7560a99c33a0bec30ad01fabe63ce938a580
Mark Lockett <mark@squarecode.com.au>
2021-08-22 21:34:04 +0800

    Update to Mvvmcross 8

--- ExampleApp.Forms.UWP/Setup.cs
+++ ExampleApp.Forms.UWP/Setup.cs
@@ -1,5 +1,7 @@
-﻿using MvvmCross;
+﻿using Microsoft.Extensions.Logging;
 using MvvmCross.Forms.Platforms.Uap.Core;
+using Serilog.Extensions.Logging;
+using Serilog;
 
 namespace ExampleApp.Forms.UWP
 {
@@ -8,6 +10,22 @@ namespace ExampleApp.Forms.UWP
         public override void InitializeSecondary()
         {
             base.InitializeSecondary();
+        }
+
+        protected override ILoggerProvider CreateLogProvider()
+        {
+            return new SerilogLoggerProvider();
+        }
+
+        protected override ILoggerFactory CreateLogFactory()
+        {
+            // serilog configuration
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                // add more sinks here
+                .CreateLogger();
+
+            return new SerilogLoggerFactory();
         }
     }
 }
