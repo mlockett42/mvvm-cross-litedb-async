0b56e5341092158d84236e8b096311fe7e4edc41
Mark Lockett <mark@squarecode.com.au>
2021-08-22 23:10:07 +0800

    Another attempt - just doesn't work app won't start

---
+++ ExampleApp.Forms.AndroidApp/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+using MvvmCross.Forms.Platforms.Android.Core;
+using MvvmCross.Forms.Platforms.Android.Views;
+
+namespace ExampleApp.Forms.AndroidApp
+{
+    //[Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity(
+        Label = "LiteDB.Async Mvvmcross",
+        Icon = "@mipmap/ic_launcher",
+        Theme = "@style/AppTheme",
+        // MainLauncher = true, // No Splash Screen: Uncomment this lines if removing splash screen
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
+        LaunchMode = LaunchMode.SingleTask)]
+    //public class MainActivity : MvxFormsAppCompatActivity<MvxFormsAndroidSetup<Core.App, UI.FormsApp>, Core.App, UI.FormsApp>
+    public class MainActivity : MvxFormsAppCompatActivity<Setup, Core.App, UI.FormsApp>
+    {
+        public MainActivity() : base()
+        {
+            
+        }
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            //SetContentView(Resource.Layout.activity_main);
+        }
+        //public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        //{
+        //    Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        //    base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        //}
+    }
+}
---
+++ ExampleApp.Forms.AndroidApp/Setup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using MvvmCross.Forms.Platforms.Android.Core;
+using Serilog;
+using Serilog.Extensions.Logging;
+
+namespace ExampleApp.Forms.AndroidApp
+{
+    public class Setup : MvxFormsAndroidSetup<Core.App, UI.FormsApp>
+    {
+        protected override ILoggerProvider CreateLogProvider()
+        {
+            return new SerilogLoggerProvider();
+        }
+
+        protected override ILoggerFactory CreateLogFactory()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .CreateLogger();
+
+            return new SerilogLoggerFactory();
+        }
+    }
+}
